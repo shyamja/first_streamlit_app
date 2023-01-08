9e7177f445e599a587651b2f0c6adfa04d96f69a
shyamja <121976641+shyamja@users.noreply.github.com>
2023-01-08 13:16:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Eggs - Scrambled')
-streamlit.text('Spinach')
-streamlit.text('Blueberry')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
