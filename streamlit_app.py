8430ce0306b7354a1cf19f7c7ede73b058c5b620
shyamja <121976641+shyamja@users.noreply.github.com>
2023-01-09 10:25:19 -0500

    Update streamlit_app.py
    user entered fruit passed to Fruitvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,11 @@ streamlit.dataframe(fruits_selected)
 
 # Display the table on the page.
 streamlit.header('Furityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # json format convert to normal
